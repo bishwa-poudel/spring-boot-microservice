37fbbe5817c7a49b5119edd078697b60ff0bbd34
Bishwa Poudel <bispoul181@gmail.com>
2024-03-13 05:36:12 +0545

    Use custom environment variable instead of k8s generated one

--- currency-conversion-service/src/main/java/com/bishwa/practice/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/bishwa/practice/currencyconversionservice/CurrencyExchangeProxy.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
  * Date: 03/08/2024
  * Time: 4:29 PM
  */
-@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
+// k8s automatically create CURRENCY_EXCHANGE_SERVICE_HOST environment variable whenever the service is up, and allows service discovery through this env variable. But better to use custom env variable because what if a service is down at a particular moment. No env variable is found.
+//@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
+@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_URI:http://localhost}:8000")
+
 public interface CurrencyExchangeProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     CurrencyConversion retrieverExchangeValue(@PathVariable String from, @PathVariable String to);
